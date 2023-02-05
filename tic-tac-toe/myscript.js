255863ca9c14294f13c2fbac932a8a6d84ce6677
larsen <larsen.cundric@longevize.com>
2023-02-05 11:08:52 +0100

    tic-tac-toe

---
+++ tic-tac-toe/myscript.js
@@ -0,0 +1,27 @@
+let game = [];
+
+const table = document.querySelector('table');
+const restart = document.getElementById('restart');
+const cells = [...document.querySelectorAll('td')];
+
+const getNewSymbol = (sym) => {
+    if (sym === '') return 'X';
+    else if (sym === 'X') return 'O';
+    return '';
+};
+
+table.addEventListener('click', function (e) {
+    const i = e.target.dataset.num;
+    const newSym = getNewSymbol(game[i]);
+    game[i] = newSym;
+    e.target.textContent = newSym;
+});
+
+const restartFunc = (e) => {
+    game = Array.from({ length: 9 }, () => '');
+    cells.forEach((td) => td.textContent = '');
+};
+
+restart.addEventListener('click', restartFunc);
+
+restartFunc();
